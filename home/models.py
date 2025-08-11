d15978bfa7dfa0839cdb152db1b3a5ddba1a3296
Gabriel Effangha <132426764+Geff115@users.noreply.github.com>
2025-08-12 00:41:54 +0100

    27730

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Creating the Restaurant model.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=200, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
