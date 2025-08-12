8fc9f1ada21952d2f876adde5d7aaf434e80e12f
Gabriel Effangha <132426764+Geff115@users.noreply.github.com>
2025-08-12 22:18:41 +0100

    27732

--- models.py
+++ models.py
@@ -5,8 +5,8 @@ class Restaurant(models.Model):
     Restaurant model
     """
     name = models.CharField(max_length=255)
-    location = models.Charfield(max_length=255, blank=True)
-    created_at = models.DateTimeField(auto_add_now=True)
+    location = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
