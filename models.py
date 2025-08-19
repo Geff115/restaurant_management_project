1b1cd560219c0d97db822d125fcc10d5e4b2444f
Gabriel Effangha <132426764+Geff115@users.noreply.github.com>
2025-08-19 16:09:16 +0100

    35457

--- models.py
+++ models.py
@@ -11,3 +11,22 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    """
+    Order model
+    """
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN_PROGRESS', 'In progress'),
+        ('COMPLETED', 'Completed'),
+        ('CANCELLED', 'Cancelled'),
+    ]
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    order_items = models.ManyToManyField(Menu, related_name="orders")
+    total_amount = models.DecimalField(max_digits=8, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer.username}"
